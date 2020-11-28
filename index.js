5770ae70c243c4e32b502584f218e5bf00b50689
EL HIDAM Loubna <loubna.elhidam@gmail.com>
2020-11-28 23:57:02 +0100

    Assignment 1: Node Modules, Express and REST API

--- index.js
+++ index.js
@@ -3,25 +3,34 @@ const http = require('http');
 const morgan = require('morgan'); 
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
-// =========================================================
-const hostname ='localhost';
-const port = 3000;
+// Express Setup
 const app =express();
-// ==========================================================
 
+// Logger
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use('/dishes', dishRouter);
 
-// ==============================================================================
+// Body Parser
+app.use(bodyParser.json());
+
+// Static Files
 app.use(express.static(__dirname+'/public'));
 
+// Routes
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 app.use((req, res, next)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type','text/html');
     res.end('<html><body><h1> This is an Express Server </h1></body></html>');
 });
+
+// Server Setup
+const hostname ='localhost';
+const port = 3000;
 const server = http.createServer(app);
 server.listen(port,hostname,()=>{
     console.log(`Server running at http://${hostname}:${port}`);
